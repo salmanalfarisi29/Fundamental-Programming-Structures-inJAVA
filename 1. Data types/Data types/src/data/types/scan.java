a6a4904efdd5195eb5e42b0ab533fba0e02e5cf0
salmanalfarisi29 <salmannalfarisii994@gmail.com>
2022-09-01 09:45:39 +0700

    .

---
+++ 1. Data types/Data types/src/data/types/scan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data.types;
+
+/**
+ *
+ * @author Salman Alfarisi
+ */
+class scan {
+
+    static long nextLong() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
